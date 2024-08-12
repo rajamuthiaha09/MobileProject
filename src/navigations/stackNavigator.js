99450f1a5373de5c1b4b5dbe6452050097ceb79d
rajahamuthiaha <sairajchellappan@gmail.com>
2024-08-12 13:25:34 +0530

    updates

--- src/navigations/stackNavigator.js
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-const Stack = createStackNavigator();
-const StackNavigator = () => {}
-
-export default StackNavigator;
